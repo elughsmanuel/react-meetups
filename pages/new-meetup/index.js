8021aea3b1887599837150fd1132a632e7b0b301
Emmanuel Elugbaju <elugbajuemmanueltobi@gmail.com>
2022-09-19 17:43:11 +0100

    Http request to api route and mongodb

--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,11 +1,28 @@
 // our-domain/new-meetup
-
+import { useRouter } from 'next/router';
 import NewMeetUpForm from '../../components/meetups/NewMeetupForm';
 
 function NewMeetupPage() {
-    function addMeetUpHandler(enteredMeetUpData) {
-        console.log(enteredMeetUpData);
+    const router = useRouter();
+
+    async function addMeetUpHandler(enteredMeetUpData) {
+        const response = await fetch('/api/new-meetup', { 
+            method: 'POST',
+            body: JSON.stringify(enteredMeetUpData),
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        });
+
+        const data = await response.json();
+        console.log(data);
+
+        router.push('/');
     }
+
+    // function addMeetUpHandler(enteredMeetUpData) {
+    //     console.log(enteredMeetUpData);
+    // }
 
     return(
         <NewMeetUpForm onAddMeetup={addMeetUpHandler} />
